944c1d677180903f7edfed7d50bda40d920b8d41
cariocaphil <cariocaphil@gmail.com>
2021-12-16 17:37:37 +0100

    extract calculateBMI

--- bmi.go
+++ bmi.go
@@ -8,8 +8,12 @@ func main() {
 
 	weight, height := getUserMetrics()
 
-	bmi := weight / (height * height)
+	bmi := calculateBMI(weight, height)
 
 	fmt.Printf("Your BMI is: %.2f ", bmi)
 
+}
+
+func calculateBMI(weight float64, height float64) float64 {
+	return weight / (height * height)
 }

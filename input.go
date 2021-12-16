dddc31e3a19e44e95a24769ba8c50b20b197efec
cariocaphil <cariocaphil@gmail.com>
2021-12-16 18:01:22 +0100

    remove code duplication

--- input.go
+++ input.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"reflect"
 	"runtime"
 	"strconv"
 	"strings"
@@ -15,24 +14,20 @@ import (
 var reader = bufio.NewReader(os.Stdin)
 
 func getUserMetrics() (float64, float64) {
-	fmt.Println(info.WeightPrompt)
-	weightInput, _ := reader.ReadString('\n')
-	fmt.Println(info.HeightPrompt)
-	heightInput, _ := reader.ReadString('\n')
 
-	// Save that user input in variables
-	if runtime.GOOS == "windows" {
-		weightInput = strings.Replace(weightInput, "\r\n", "", -1)
-		heightInput = strings.Replace(heightInput, "\r\n", "", -1)
-	} else {
-		weightInput = strings.Replace(weightInput, "\n", "", -1)
-		heightInput = strings.Replace(heightInput, "\n", "", -1)
-	}
-
-	weight, _ := strconv.ParseFloat(weightInput, 64)
-	height, _ := strconv.ParseFloat(heightInput, 64)
-
-	fmt.Println(reflect.TypeOf(weight))
-
+	weight := getUserInput(info.WeightPrompt)
+	height := getUserInput(info.HeightPrompt)
 	return weight, height
+}
+
+func getUserInput(promptText string) float64 {
+	fmt.Println(promptText)
+	userInput, _ := reader.ReadString('\n')
+	if runtime.GOOS == "windows" {
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+	} else {
+		userInput = strings.Replace(userInput, "\n", "", -1)
+	}
+	value, _ := strconv.ParseFloat(userInput, 64)
+	return value
 }

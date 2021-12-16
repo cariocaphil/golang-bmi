6cd7490424e2666772d28f5e7e8415a3d941677c
cariocaphil <cariocaphil@gmail.com>
2021-12-16 12:30:53 +0100

    add cleaning and parsing of input

--- bmi.go
+++ bmi.go
@@ -4,6 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"reflect"
+	"runtime"
+	"strconv"
+	"strings"
 )
 
 var reader = bufio.NewReader(os.Stdin)
@@ -16,7 +20,21 @@ func main() {
 	fmt.Println("Please enter your height (m): ")
 	heightInput, _ := reader.ReadString('\n')
 
-	fmt.Println(weightInput)
-	fmt.Println(heightInput)
+	// Save that user input in variables
+	if runtime.GOOS == "windows" {
+		weightInput = strings.Replace(weightInput, "\r\n", "", -1)
+		heightInput = strings.Replace(heightInput, "\r\n", "", -1)
+	} else {
+		weightInput = strings.Replace(weightInput, "\n", "", -1)
+		heightInput = strings.Replace(heightInput, "\n", "", -1)
+	}
+
+	weight, _ := strconv.ParseFloat(weightInput, 64)
+	height, _ := strconv.ParseFloat(heightInput, 64)
+
+	fmt.Println(reflect.TypeOf(weight))
+
+	fmt.Println(weight)
+	fmt.Println(height)
 
 }
